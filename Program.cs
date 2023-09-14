cee2b67480181ed1bd7708955ffa2afe9b036065
Soma Mayer <mayersoma2@gmail.com>
2023-09-14 14:16:22 +0200

    Async and LINQ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Running;
+using DotNet.Async;
 
+#region Async
+
+//var results = BenchmarkRunner.Run<ThreadTasks>();
+//ThreadTasks test = new ThreadTasks();
+
+//var test2 = await test.GetWebsitesAsync();
+//var test3 = test.GetWebsitesSync();
+
+//var test4 = test.GetWebsiteContentParallel3();
+//var test5 = test.GetWebsiteContentParallel10();
+
+//foreach (var item in test4) 
+//{
+//    Console.WriteLine(item.Length);
+//};
+
+# endregion Async
+
+# region LINQ
+
+//var linq = new LinqPlays();
+//linq.Play();
+
+#endregion LINQ
+
+#region
